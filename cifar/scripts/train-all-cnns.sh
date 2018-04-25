02b72eb2ce53f47e6520ef1ec358e91e1f60c0cc
wangjksjtu <wangjksjtu_01@sjtu.edu.cn>
2018-04-25 17:26:05 +0800

    Fix a spelling mistake

--- cifar/scripts/train-all-cnns.sh
+++ cifar/scripts/train-all-cnns.sh
@@ -1,5 +1,5 @@
 (export CUDA_VISIBLE_DEVICES=0 && python train.py --quality 1 > logs/quality_1.log)&
-(export CUDA_VISIBLE_DEVICES=1 && python train.py --quality 5 > logs/qualtiy_5.log)&
+(export CUDA_VISIBLE_DEVICES=1 && python train.py --quality 5 > logs/quality_5.log)&
 (export CUDA_VISIBLE_DEVICES=2 && python train.py --quality 10 > logs/quality_10.log)&
 (export CUDA_VISIBLE_DEVICES=3 && python train.py --quality 15 > logs/quality_15.log)&
 (export CUDA_VISIBLE_DEVICES=4 && python train.py --quality 20 > logs/quality_20.log)&
